8e421f4623d285dc8e13e4246221d9ec9dd433e2
samiulalim1 <47431474+samiulalim1@users.noreply.github.com>
2023-10-09 11:35:54 +0600

    Create php-encode-emoji.php

---
+++ test/php-encode-emoji.php
@@ -0,0 +1,18 @@
+<?php
+//include mr.encryptor.min.php
+include '../src/mr.encryptor.min.php';
+
+//use of MrEncode wirh Emoji encode in php
+//string which you want to encode 
+$string = 'Hello World';
+//output result emoji/string
+$emoji = true;
+//encode mode > 0
+$mode = 3;
+//encryption
+$data = MrEncode($string, $emoji, $mode);
+
+//output
+echo $data; //data => 🍀😓🐻🍎👌🐶🔥😂🐻💣🐟😉🍎🐶🍻⚽😱💘😝💩😝🍸🐟🍎💄🧐👊💩😝🍎🐻🍎💄🍎🎉😂🐩🚗🙌👠🎾🚗☀🌈💩👠🎾👂🤪👠🐻⚽💩🚗🐟👿😝💜👊🍎🎾💘🏈🎀
+
+?>
