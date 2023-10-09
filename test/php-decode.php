8dbc0cbc5f18c564b0fdc865725f42ccb73ebfa5
samiulalim1 <47431474+samiulalim1@users.noreply.github.com>
2023-10-09 11:44:46 +0600

    Update php-decode.php

--- test/php-decode.php
+++ test/php-decode.php
@@ -2,7 +2,7 @@
 //include mr.encryptor.min.php
 include '../src/mr.encryptor.min.php';
 
-//use of MrDecode with Emoji decode in php
+//use of MrDecode in php
 //string/emoji which you want to decode 
 $string = 'Y8U14VIBUtX71VHPExCbCcX1NvzbC1U1N1gt53QKg=hJif0=N/Xc5tTTCyz1QxkO';
 //encode result emoji/string
